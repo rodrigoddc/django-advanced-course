84bf40c93ca9c31e0440027c7c6d77ce81e358ec
Rodrigo Delfino de Carvalho <rodrigoddc1@gmail.com>
2020-06-30 11:50:41 -0300

    (done) created base app
    close #5

--- pypro/urls.py
+++ pypro/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from apps.base import urls as base_urls
 
 urlpatterns = [
+    path('', include(base_urls)),
     path('admin/', admin.site.urls),
 ]
