a04f7a29e85ac23e20d017605be1e0365a4a5b38
Rodrigo Delfino de Carvalho <rodrigoddc1@gmail.com>
2020-07-02 11:02:35 -0300

    (done) added Sentry

--- pypro/settings.py
+++ pypro/settings.py
@@ -18,8 +18,6 @@ import sentry_sdk
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from sentry_sdk.integrations.django import DjangoIntegration
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
